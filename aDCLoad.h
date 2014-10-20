103dbe1f381cbc81e932ca42f34462b6db91b0c3
Daniel Caujolle-Bert <danielvador@gmail.com>
2014-10-20 15:42:00 +0200

    Add licences

--- aDCLoad.h
+++ aDCLoad.h
@@ -1,3 +1,22 @@
+/*
+
+  Copyright (C) 2014  Lee Wiggins <lee@wigweb.com.au>
+                      F1RMB, Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 #ifndef ADCLOAD_H
 #define ADCLOAD_H
 
@@ -5,7 +24,7 @@
 #include <LiquidCrystal.h>
 #include <ClickEncoder.h>
 
-#define SIMU 1
+//#define SIMU 1
 #define RESISTANCE 1
 
 // Set Constants
--- sketch.cpp
+++ sketch.cpp
@@ -1,3 +1,23 @@
+/*
+
+  Copyright (C) 2014  Lee Wiggins <lee@wigweb.com.au>
+                      F1RMB, Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <Arduino.h>
 
 #include "aDCLoad.h"
@@ -15,9 +35,9 @@ Initial upload
 
 2.0
 * full rewrite, using C++. Lot of bug fixes, lot of new features.
-* size of the hex should be
-*     text	   data	    bss	    dec	    hex	filename
-*     30792	    206	    506	  31504	   7b10	output/sketch.elf
+* size of the hex should be:
+*      text	   data	    bss	    dec	    hex	filename
+*     30884	    206	    506	  31596	   7b6c	output/sketch.elf
 
 To - Dos
 * Display in engineering units, ie. mA and mW.
