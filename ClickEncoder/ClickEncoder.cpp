fe9f6b1ae1be1f591574330312da56f0d608604f
Daniel Caujolle-Bert <danielvador@gmail.com>
2014-12-06 09:27:26 +0100

    Fix stupid button pin checking.

--- ClickEncoder/ClickEncoder.cpp
+++ ClickEncoder/ClickEncoder.cpp
@@ -132,7 +132,7 @@ void ClickEncoder::service(void)
   static uint8_t doubleClickTicks = 0;
   static unsigned long lastButtonCheck = 0;
 
-  if (pinBTN > 0 // check button only, if a pin has been provided
+  if ((pinBTN != -1) // check button only, if a pin has been provided
       && (now - lastButtonCheck) >= ENC_BUTTONINTERVAL) // checking button is sufficient every 10-30ms
   {
     lastButtonCheck = now;
